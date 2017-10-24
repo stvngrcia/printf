d2a2d492296257851a5a26b51863d1944cc4a699
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-23 19:42:27 -0700

    trying to handle 0 for binary input

--- base_changes.c
+++ base_changes.c
@@ -15,7 +15,7 @@ int print_binary(va_list list)
 	num = va_arg(list, unsigned int);
 	if (num == 0)
 		return (_write_char('0'));
-	if (num < 0)
+	if (num < 1)
 		return (-1);
 	len = b_length(num);
 	str = malloc(sizeof(char) * len + 1);
